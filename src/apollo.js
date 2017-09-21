1ec7a6f7757e128b08009b32268e4bd9b1b5f59e
Nick Bushak <nbushak@gmail.com>
2017-09-21 11:09:49 -0700

    update to apollo2

--- src/apollo.js
+++ src/apollo.js
@@ -1,12 +1,10 @@
-import {ApolloClient, createNetworkInterface} from 'react-apollo'
-
-const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/cj697b1ut4w060187xfsjfcyz',
-  // opts: {
-  //   credentials: 'include',
-  // },
-})
+import ApolloClient from 'apollo-client'
+import Link from 'apollo-link-http';
+import Cache from 'apollo-cache-inmemory'
 
 export const client = new ApolloClient({
-  networkInterface,
+  link: new Link({
+    uri: 'https://api.graph.cool/simple/v1/cj697b1ut4w060187xfsjfcyz',
+  }),
+  cache: new Cache(),
 })
